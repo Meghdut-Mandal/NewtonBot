efe6759f1d499b8fae90318a15d7dc41b6bd2b3d
Meghdut Mandal <meghdut.mandal@autodesk.com>
2022-04-26 18:22:42 +0530

    Improved memes.js performance

--- src/modules/meme.js
+++ src/modules/meme.js
@@ -1,33 +1,55 @@
 const STRINGS = require("../lib/db");
 const https = require("https");
 
+var memeData = null // array of meme objects
+
+
 module.exports = {
     name: "meme",
     description: STRINGS.meme.DESCRIPTION,
     extendedDescription: STRINGS.meme.EXTENDED_DESCRIPTION,
     demo: {isEnabled: true, text: ['.meme ']},
     async handle(client, chat, BotsApp, args) {
-        const proccessing = await client.sendMessage(BotsApp.chatId,
+        await client.sendMessage(BotsApp.chatId,
             {
                 text: STRINGS.meme.PROCESSING
             });
 
-        // Get the meme
-        await https.get("https://www.reddit.com/r/dankmemes.json?sort=top&t=week&limit=60", res => {
-            let body = "";
-            res.on("data", data => {
-                body += data;
-            });
-            res.on("end", () => {
-                const json = JSON.parse(body);
-                const meme = json.data.children[Math.floor(Math.random() * json.data.children.length)].data.url;
-                console.log(" The meme link is"+meme);
-                client.sendMessage(BotsApp.chatId, {
-                    image: {
-                        url: meme
-                    }
+        // get the meme data
+        async function getMemeData() {
+            if (!memeData) {
+                await https.get("https://www.reddit.com/r/dankmemes.json?sort=top&t=week&limit=60", res => {
+                    let body = "";
+                    res.on("data", data => {
+                        body += data;
+                    });
+                    res.on("end", () => {
+                        memeData = JSON.parse(body);
+                        console.log("Meme Data loaded " + JSON.stringify(memeData));
+                    });
                 });
+            }
+        }
+
+        await getMemeData(); // get meme data from reddit
+
+        const meme = memeData.data.children[Math.floor(Math.random() * memeData.data.children.length)].data;
+        console.log(" The meme link is" + meme);
+
+        // check if the meme is a video
+        if (meme.is_video){
+            client.sendMessage(BotsApp.chatId, {
+                video: {
+                    url: meme.url
+                }
             });
-        });
+        }else {
+            client.sendMessage(BotsApp.chatId, {
+                image: {
+                    url: meme.url
+                }
+            });
+        }
+
     }
 }
